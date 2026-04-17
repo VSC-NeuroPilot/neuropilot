2aca289cde20afc8e0dc44f3e89c6d40a2b68a1e
KTrain5369 <me@git.ktrain5369.dev>
2026-04-17 15:53:06 +1000

    add enum temporarily

---
+++ packages/types/src/companions/enum.ts
@@ -0,0 +1,12 @@
+export const enum CONTRIBUTIONS {
+    ACTIONS_MANAGE = 'actions:manage',
+    ACTIONS_MANAGE_OTHERS = 'actions:manage_others',
+    ACTIONS_INJECT = 'actions:inject',
+    ACTIONS_PROCESS = 'actions:process',
+    ACTIONS_FORCE = 'actions:force',
+    CHANGELOG = 'changelog',
+    CONTEXT = 'context',
+    CURSOR_GET = 'cursor:get',
+    CURSOR_SET = 'cursor:set',
+    IMAGES = 'images',
+};
--- packages/types/src/companions/index.ts
+++ packages/types/src/companions/index.ts
@@ -1,2 +1,3 @@
 export * from './errors';
 export type * from './register.d';
+export * from './enum';
