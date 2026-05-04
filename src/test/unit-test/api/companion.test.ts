0336d8a415b10de98ea0512dda6bf0062e134409
KTrain5369 <me@git.ktrain5369.dev>
2026-05-04 10:08:31 +1000

    fix failing tests + add test watching config

--- src/test/unit-test/api/companion.test.ts
+++ src/test/unit-test/api/companion.test.ts
@@ -5,13 +5,13 @@ import assert, { AssertionError } from 'assert';
 import { changelogs } from '@/changelog';
 import { registry } from '@/plugins';
 
-const testCompanionMetadata = { name: 'NeuroPilot Unit Tests', author: 'VSC-NeuroPilot', extensionId: 'vsc-neuropilot.neuropilot-base', contributes: [Contributions.CONTEXT] };
+const testCompanionMetadata = { name: 'NeuroPilot Unit Tests', author: 'VSC-NeuroPilot', extensionId: 'vsc-neuropilot.neuropilot-base', contributes: [Contributions.CHANGELOG] };
 
 suite('Validation decorator suite', () => {
-    const companion = new Companion(testCompanionMetadata);
+    const companion = new Companion({ ...testCompanionMetadata, name: testCompanionMetadata.name + `-${Math.random()}` });
     test('Can add to changelog', () => {
         companion.addChangelog('under test', 'Currently under test.');
-        assert(changelogs[companion['token']].find((c) => c.version === 'under test' && c.body === 'Currently under test.'));
+        assert(changelogs[companion['data'].name].find((c) => c.version === 'under test' && c.body === 'Currently under test.'));
     });
     test('Cannot call action functions', () => {
         try {
@@ -28,9 +28,12 @@ suite('Validation decorator suite', () => {
 
 suite('Companion setup and teardown', () => {
     test('Added and removed to companion registry', () => {
-        const companion = new Companion(testCompanionMetadata);
-        assert(registry[companion['token']] === testCompanionMetadata);
+        const metadata = { ...testCompanionMetadata, name: testCompanionMetadata.name + `-${Math.random()}` };
+        const companion = new Companion(metadata);
+        console.log(companion['token']);
+        console.log(JSON.stringify(registry));
+        assert(registry[companion['token']].name === metadata.name);
         companion.dispose();
-        assert(!Object.values(registry).includes(testCompanionMetadata));
+        assert(!Object.values(registry).includes(metadata));
     });
 });
