fc9bfe7bff6726a030d40b3854019ac9f69c29eb
KTrain5369 <me@git.ktrain5369.dev>
2026-05-04 10:57:52 +1000

    do more CLI work to start getting it ready

--- packages/cli/src/commands/test.ts
+++ packages/cli/src/commands/test.ts
@@ -3,13 +3,31 @@ import { defineCommand } from 'citty';
 export const testCommand = defineCommand({
     meta: {
         name: 'test',
-        description: 'Install and open the integration test extension in VS Code.',
-        hidden: true,
+        description: 'Install and open the integration test extension in VS Code',
+        alias: ['integration-test'],
     },
     args: {
-        version: {},
-        github: {},
-        vscode: {},
+        version: {
+            type: 'positional',
+            description: 'The integration test extension to download.',
+            required: true,
+        },
+        type: {
+            type: 'enum',
+            description: 'The test type.',
+            required: true,
+            options: ['base', 'companions'],
+        },
+        github: {
+            type: 'boolean',
+            description: 'Download from GitHub instead of VS Marketplace.',
+            default: false,
+        },
+        vscode: {
+            type: 'enum',
+            description: 'The build to install into using its CLI.',
+            options: ['stable', 'insiders'],
+        },
     },
-    run(ctx) {},
+    async run(ctx) {},
 });
