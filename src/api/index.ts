e7b09454b2e8690dd92827b9b6fa86b3c938fad8
Pasu4 <pascalwieckhorst02@gmail.com>
2026-06-28 12:41:09 +0200

    Update the implementation

--- src/api/index.ts
+++ src/api/index.ts
@@ -44,74 +44,64 @@ class Config implements NeuroPilotConfig {
 
 export const api: NeuroPilotAPI = {
     Companion,
-    utils: {
-        actionValidation: {
-            success: actionValidationAccept,
-            failure: actionValidationFailure,
-            retry: actionValidationRetry,
-        },
-        actionHandler: {
-            success: actionHandlerSuccess,
-            failure: actionHandlerFailure,
-            retry: actionHandlerRetry,
-        },
-        actionsListing: {
-            getActions(action?: string | string[]) {
-                if (typeof action === 'string') {
-                    const foundAction = getAction(action);
-                    if (!foundAction) return foundAction;
-                    const actionReturn = {...foundAction, source: findByToken(foundAction.sourceToken)?.name };
-                    delete actionReturn?.sourceToken;
-                    return actionReturn;
-                } else {
-                    const actions = getActions(action);
-                    return actions.map((a) => {
-                        const newObject = {...a, source: findByToken(a.sourceToken)?.name };
-                        delete newObject.sourceToken;
-                        return newObject;
-                    });
-                };
-            },
-        },
-        context: {
-            getPositionContext,
-            formatContext,
-        },
-        diffs: {
-            calculateDiff(oldLines: string[], newLines: string[]) {
-                const diff = patienceDiff(oldLines, newLines);
-                return {
-                    lines: diff.lines.map(line => ({
-                        text: line.line,
-                        oldIndex: line.aIndex,
-                        newIndex: line.bIndex,
-                    })),
-                    lineCountDeleted: diff.lineCountDeleted,
-                    lineCountInserted: diff.lineCountInserted,
-                } satisfies Diff;
-            },
-            calculateDiffPlus(oldLines: string[], newLines: string[]) {
-                const diff = patienceDiffPlus(oldLines, newLines);
-                return {
-                    lines: diff.lines.map(line => ({
-                        text: line.line,
-                        oldIndex: line.aIndex,
-                        newIndex: line.bIndex,
-                        moved: line.moved ?? false,
-                    })),
-                    lineCountDeleted: diff.lineCountDeleted,
-                    lineCountInserted: diff.lineCountInserted,
-                    lineCountMoved: diff.lineCountMoved,
-                } satisfies DiffPlus;
-            },
-            calculateDiffRanges: getDiffRanges,
-            applyDiffHighlighting(editor: vscode.TextEditor, diffRanges: DiffRange[]) {
-                showDiffRanges(editor, ...diffRanges);
-            },
-        },
-        filePaths: {
-            isPathNeuroSafe,
-        },
-    },
     config: new Config(),
+    actionValidation: {
+        success: actionValidationAccept,
+        failure: actionValidationFailure,
+        retry: actionValidationRetry,
+    },
+    actionHandler: {
+        success: actionHandlerSuccess,
+        failure: actionHandlerFailure,
+        retry: actionHandlerRetry,
+    },
+    getActions(action?: string | string[]) {
+        if (typeof action === 'string') {
+            const foundAction = getAction(action);
+            if (!foundAction) return foundAction;
+            const actionReturn = {...foundAction, source: findByToken(foundAction.sourceToken)?.name };
+            delete actionReturn?.sourceToken;
+            return actionReturn;
+        } else {
+            const actions = getActions(action);
+            return actions.map((a) => {
+                const newObject = {...a, source: findByToken(a.sourceToken)?.name };
+                delete newObject.sourceToken;
+                return newObject;
+            });
+        };
+    },
+    getPositionContext,
+    formatContext,
+    calculateDiff(oldLines: string[], newLines: string[]) {
+        const diff = patienceDiff(oldLines, newLines);
+        return {
+            lines: diff.lines.map(line => ({
+                text: line.line,
+                oldIndex: line.aIndex,
+                newIndex: line.bIndex,
+            })),
+            lineCountDeleted: diff.lineCountDeleted,
+            lineCountInserted: diff.lineCountInserted,
+        } satisfies Diff;
+    },
+    calculateDiffPlus(oldLines: string[], newLines: string[]) {
+        const diff = patienceDiffPlus(oldLines, newLines);
+        return {
+            lines: diff.lines.map(line => ({
+                text: line.line,
+                oldIndex: line.aIndex,
+                newIndex: line.bIndex,
+                moved: line.moved ?? false,
+            })),
+            lineCountDeleted: diff.lineCountDeleted,
+            lineCountInserted: diff.lineCountInserted,
+            lineCountMoved: diff.lineCountMoved,
+        } satisfies DiffPlus;
+    },
+    calculateDiffRanges: getDiffRanges,
+    applyDiffHighlighting(editor: vscode.TextEditor, diffRanges: DiffRange[]) {
+        showDiffRanges(editor, ...diffRanges);
+    },
+    isPathNeuroSafe,
 };
